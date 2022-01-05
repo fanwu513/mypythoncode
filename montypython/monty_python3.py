a465e9e489546bb95279a1d15beacabeb51f3106
Fan Wu <fanwu513@users.noreply.github.com>
2022-01-05 23:04:22 +0000

    while looping

--- montypython/monty_python3.py
+++ montypython/monty_python3.py
@@ -8,6 +8,7 @@ answer = " "
 while round < 3 and answer != "Brian":
     round += 1     # increase the round counter by 1
     answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
+    answer = answer.capitalize()
     if answer == "Brian": # logic to check if user gave correct answer
         print("Correct!")
     elif answer == "Shrubbery":
